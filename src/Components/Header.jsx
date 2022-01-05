d56244dfedff3debcffb61ce15c8e6737062974f
Dungyy <erickmunoz13@gmail.com>
2022-01-05 00:36:11 -0600

    minor updates to Keeper App

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
 
-
 function Header() {
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1>Keeper</h1><h3 style={{color:"white"}}>of your memories</h3>
         </header>
     )
 };
