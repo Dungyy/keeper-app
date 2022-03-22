f9f04eeefebfc2c42523152758c94e6a767e72ac
Dungyy <erickmunoz13@gmail.com>
2022-03-22 01:08:48 -0500

    minor css issues

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
-// import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 import { v4 as uuidv4 } from "uuid";
+import HeroBG from "./HeroBG";
+// import Home from "../pages/Home";
 
 export default function App() {
   const [notes, setNotes] = useState([]);
@@ -24,20 +26,27 @@ export default function App() {
 
   return (
     <div>
-      <Header />
-      <CreateArea onAdd={addItem} />
-      {notes.map((noteText, index) => {
-        return (
-          <Note
-            key={uuidv4()}
-            id={index}
-            onDelete={deleteNote}
-            title={noteText.title}
-            content={noteText.content}
-          />
-        );
-      })}
-      {/* <Footer /> */}
+      
+        <Header />
+        <HeroBG />
+        {/* <Router>
+        <NavLink>
+          <Route exact path="/" component={Home} />
+        </NavLink>
+        </Router> */}
+        <CreateArea onAdd={addItem} />
+        {notes.map((noteText, index) => {
+          return (
+            <Note
+              key={uuidv4()}
+              id={index}
+              onDelete={deleteNote}
+              title={noteText.title}
+              content={noteText.content}
+            />
+          );
+        })}
+      
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroBG from "../Components/HeroBG";
+
+function Home() {
+  return (
+    <>
+      <HeroBG />
+      
+    </>
+  );
+}
+
+export default Home;
